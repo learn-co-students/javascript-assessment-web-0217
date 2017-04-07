5b789ab2cf08d517404222957015e596f2e93bb1
Your Name <you@example.com>
2017-04-07 11:41:18 -0400

    Done.

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -4,10 +4,21 @@ class CommentsController {
   }
 
   init() {
+
     // kick off controller from here
   }
 
   addCommentFormListener() {
+    //copied from stqackoverflow need to be refactored for this
+    // in the ul data-id= unique id
+    document.getElementById('comment-description').addEventListener("submit", function() {
+       for (var i = 0, j = this.childNodes.length; i < j, i++) {
+           if (this.childNodes[i].value !== "") {
+               return (this.childNodes[i].id);
+
+           }
+       }
+   }, false);
     // create comment form listener code here
   }
 }

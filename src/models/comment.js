26fec03fc49afd985bafc07cc6a08fa7ce519798
mwei2509 <m2dl89@gmail.com>
2017-04-07 11:30:01 -0400

    Done.

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,19 @@
 // create Comment class here
+class Comment{
+  constructor(imageid,comment){
+    this.id = this.constructor.all.length
+    this.imageid = imageid
+    this.message = comment
+    this.constructor.all.push(this);
+  }
+
+  findImage(imageId){
+    let image=Image.all[imageId]
+    image.comments.push(this)
+  }
+
+  commentEl(){
+    return `<li id=${this.id}>${this.message}</li>`
+  }
+}
+Comment.all=[]

96621661550c237097f5d677e0caa5b946bfb309
Devin Rieger <dev0089@gmail.com>
2017-04-07 11:30:38 -0400

    Done.

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,27 @@
 // create Comment class here
+class Comment {
+  constructor(comment, imageId) {
+    this.id = this.constructor.all.length
+    this.commentContent = comment
+    this.image = this.findImage(imageId)
+    this.save()
+  }
+  findImage(imageId) {
+    let foundImage = Image.all.filter(function(image) {
+      return image.id == imageId
+    })[0]
+    foundImage.comments.push(this)
+    return foundImage
+  }
+  save() {
+    allComments.push(this)
+  }
+  commentEl() {
+    return `<li id=${this.id}>${this.commentContent}</li>`
+  }
+  static all() {
+    return allComments
+  }
+}
+
+var allComments = []

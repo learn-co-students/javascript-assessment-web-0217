b692e90f3fc35f1338da7fb6689f9b150521cafa
jef4490 <jeffrey.hechler@gmail.com>
2017-04-07 11:30:27 -0400

    done

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -4,10 +4,24 @@ class CommentsController {
   }
 
   init() {
-    // kick off controller from here
+    this.addCommentFormListener()
   }
 
   addCommentFormListener() {
-    // create comment form listener code here
+    $('.add-comment').toArray().forEach((child) => {
+      child.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let commentText = event.target.children[1].value
+        let imageId = parseInt(event.target.dataset.id)
+        let comment = new Comment(commentText, imageId)
+        this.render(comment)
+        event.target.children[1].value = ""
+      })
+    })
   }
+
+  render(commentObject) {
+    $(`#image-${commentObject.image.id}`).append(commentObject.commentEl())
+  }
+
 }

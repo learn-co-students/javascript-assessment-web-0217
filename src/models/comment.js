a0e1dd0020aa55b1106c346d51e6db41c09779f6
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-07 10:22:36 -0400

    updates to comment class

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,24 @@
 // create Comment class here
+'use strict';
+
+function Comment(comment, imageId) {
+  this.id = imageId
+  this.findImage = function() {
+    return document.getElementById(this.id)
+  }
+  this.commentContent = function() {
+    return this.comment.toString()
+  }
+}
+
+Comment.prototype.commentEl = function() {
+  return `
+    <div class="comment">
+      <ul>
+        <li id="${this.id}">${this.commment}</li>
+      </ul>
+    </div>
+  `
+}
+
+Comment.all = []

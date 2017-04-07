2e47e185adf7974e03c3e065160ec6b36b7a3032
Alexandra Roth <alroth8@gmail.com>
2017-04-07 11:31:57 -0400

    Done.

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -4,10 +4,19 @@ class CommentsController {
   }
 
   init() {
+  	this.addCommentFormListener()
+  	this.render()
+
     // kick off controller from here
   }
 
   addCommentFormListener() {
+  		document.getElementById("add-comment").addEventListener('click', render)
+  	}
     // create comment form listener code here
+
+  render() {
+  	document.getElementById("comment-description-${comment.id}").innerHTML = newComment()
   }
+
 }

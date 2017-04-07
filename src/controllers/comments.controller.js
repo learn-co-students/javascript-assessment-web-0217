ea6c0eedcb0bd1f4bb1f0bab019d31c5e6665d32
CorinneKelly <corinnemariekelly@gmail.com>
2017-04-07 11:31:32 -0400

    i did some stuff, some things happened, some things didnt happened

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,13 +1,32 @@
 class CommentsController {
   constructor() {
     this.$addCommentForm = $('.add-comment')
+    this.addCommentFormListener = this.addCommentFormListener.bind(this)
+
   }
 
   init() {
-    // kick off controller from here
+  	this.addCommentFormListener
   }
 
   addCommentFormListener() {
-    // create comment form listener code here
+  	document.getElementById("add-comment").addEventListener("submit", function() {
+	  	let imgID = parseInt($("#add-comment").parents('ul').data('id'))
+  		
+	  	// should have been and instance of a comment instead of comment.prototype
+  		Comment.prototype.findImage(imgID).comments.forEach((commentContent) => {
+	  		let tempComment = new Comment(commentContent, this.id)
+	  		render(tempComment)
+  		})  		
+  	})
+
+
   }
+
+  render(commentObj){
+  	console.log('im rendering', commentObj.id)
+  	$("#comments-${commentObj.imageId}").append(commentObj.commentEl())
+  }
+
 }
+
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,31 @@
 // create Comment class here
+var allComments = []
+class Comment {
+	constructor(content, imageId){
+		this.content = content
+		this.imageId = imageId
+		this.commentId = this.constructor.allComments.length
+		this.addCommentToAll()
+	}
+
+	addCommentToAll(){
+		allComments.push(this)
+	}
+
+	static all(){
+		return allComments
+	}
+
+	findImage(imageId){
+		Image.all.forEach((img) => {
+			if (img.id === imageId) {
+				img.comments.push(this)
+				return img
+			}
+		})
+	}
+
+	commentEl(){
+		return '<li id="comment${this.commentId}">{this.comment}</li>'
+	}
+}

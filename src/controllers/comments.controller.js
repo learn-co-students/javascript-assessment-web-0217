7518926af272d74a84f369a668c4936c7bc2ea08
nanyr <nanyramirez0@gmail.com>
2017-04-07 11:29:35 -0400

    add comments controller and model

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,24 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+    this.addCommentFormListener()
   }
 
   addCommentFormListener() {
     // create comment form listener code here
+    $( ".add-comment" ).submit(function( event ) {
+      event.preventDefault();
+      const text=document.getElementById("comment-description-"+ this.dataset.id).value
+      const comment=new Comment (this.dataset.id, text);
+      $(`#comments-${this.imageId}`).append(this.commentContent)
+      // render(comment)
+    });
+
   }
+
+  // render(commentObject){
+  //   $(`#comments-${this.imageId}`).append(commentObject.commentContent)
+  // }
+
+
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,29 @@
 // create Comment class here
+
+class Comment{
+  constructor(commentContent, imageId){
+    this.commentContent=commentContent;
+    this.imageId=imageId
+    this.constructor.all.push(this)
+  }
+
+  static all(){
+    return this.all
+  }
+
+  findImage(imageId){
+    let image=Image.all.find((image)=>{
+      return image.id===imageId
+    })
+    image.comments.push(this)
+  }
+
+  commentEl(){
+    const html= `<li id=image${this.imageId}-comment>${this.commentContent}</li>`;
+    return html
+  }
+
+
+
+}
+Comment.all = [];

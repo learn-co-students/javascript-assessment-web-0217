5698ca60236d0403985dfa8d1ecb67c7f136acd0
dylanito2 <dylanito2@gmail.com>
2017-04-07 11:13:33 -0400

    done

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,31 @@
-// create Comment class here
+
+const allComments = []
+class Comment {
+  constructor(comment, imageId) {
+    this.imageId = imageId
+    this.comment = comment
+    allComments.push(this)
+    this.findImage(imageId)
+  }
+
+  findImage(imageId) {
+    let foundImg =  Image.all.find((img) => {
+      if (img) {
+        return img.id === imageId
+      }
+      else {
+        return null
+      }
+    })
+    foundImg.comments.push(this)
+    return foundImg
+  }
+
+  commentEl() {
+    return `<li>${this.comment}</li>`
+  }
+
+  static all() {
+    return allComments
+  }
+}

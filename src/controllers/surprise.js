5416fad36f97fe5c7c6400e0af9d9f4fa719e185
hwalborn <hwalborn@gmail.com>
2017-04-07 17:33:00 -0400

    made one small change

--- src/controllers/surprise.js
+++ src/controllers/surprise.js
@@ -1,8 +1,10 @@
 $(()=>{
+  var audio = new Audio('img/dino.wav')
+  
   $('div#surprise').click(()=>{
     $('body').addClass('dino')
+    audio.play()
 
   })
-  var audio = new Audio('img/dino.wav')
-  audio.play()
+
 })

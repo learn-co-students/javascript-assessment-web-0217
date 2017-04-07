15b5dc57d98b587aea54deed8ad4fd7c5528ab2f
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-07 11:10:43 -0400

    finished

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,26 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+    this.addCommentFormListener()
   }
 
   addCommentFormListener() {
     // create comment form listener code here
+    var self = this
+    $('form').each((i, el) => {
+      $(el).submit((event)=> {
+        event.preventDefault()
+        let imageId = $(el).attr('data-id')
+        let input = $(`#comment-description-${imageId}`)
+        let commentContent = input.val()
+        var comment = new Comment(commentContent, imageId)
+        self.render(comment)
+        input.val('')
+      })
+    })
+  }
+
+  render(comment) {
+    $(`#comments-${comment.image.id}`).append(comment.commentEl())
   }
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,23 @@
 // create Comment class here
+class Comment {
+  constructor(commentContent, imageId) {
+    Comment.saveComment(this)
+    this.id = Comment.all.length - 1
+    this.image = this.findImage(imageId)
+    this.commentContent = commentContent
+  }
+
+  static saveComment(comment) {
+    this.all ? this.all.push(comment) : this.all = [comment]
+  }
+
+  findImage(int) {
+    let image = Image.all[int]
+    image.comments.push(this)
+    return image
+  }
+
+  commentEl() {
+    return `<li id=comment-${this.id}>${this.commentContent}</li>`
+  }
+}

8fc06d35e0c490fced2e94365fa2869447d02a3e
Daniel <daniel.seehausen@gmail.com>
2017-04-07 11:31:16 -0400

    Done.

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,13 +1,44 @@
+
+
 class CommentsController {
+
   constructor() {
     this.$addCommentForm = $('.add-comment')
   }
 
   init() {
-    // kick off controller from here
+    this.addCommentFormListener()
+  }
+
+  static render(commentObject) {
+    $(`#comments-${commentObject.imageId}`).append(commentObject.commentEl())
+  }
+
+  static handleFormSubmit(event, commentText, imageId) {
+    event.preventDefault()
+    let commentObject = new Comment(commentText, imageId)
+    CommentsController.render(commentObject)
   }
 
   addCommentFormListener() {
-    // create comment form listener code here
+    this.$addCommentForm.each(function(id) {
+      let imageId = id
+      $(`#image-${id}`).submit(function(event) {
+        let commentText = $(`#comment-description-${imageId}`).val()
+        $(`#comment-description-${imageId}`).val("")
+        CommentsController.handleFormSubmit(event, commentText, imageId)
+      })
+    })
   }
+
 }
+
+//
+// CommentsController.prototype.addCommentFormListener()
+  // iterates through each comment form and adds an eventlistener to trigger a function on form submit
+  // function should grab the imageId + comment and create a new Comment with those arguments
+  // execute the render function on that found image object to append the new comment
+// CommentsController.prototype.render(commentObject)
+  // selects the appropriate ul for this comment to be added to
+  // appends the new comment element to this ul
+  // Don't try to copy the ImagesController.render function because that is implemented differently
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,41 @@
-// create Comment class here
+class Comment {
+
+  constructor(comment, imageId) {
+    this.comment = comment
+    this.imageId = imageId
+    //TODO this is not an image id, but the encapsulating UL (readme off?). should be changed
+    this.image = $(`#image-${imageId}`)
+    this.id = Comment.all.length
+    Comment.all.push(this)
+  }
+
+  static findImage(id) {
+    return Comment.all.find((element) => {
+      if (element.imageId === id) {
+        CommentsController.render(element)
+        return true
+      }
+    })
+  }
+
+  commentEl() {
+    return `<li id="${this.id}">${this.comment}</li>`
+  }
+
+}
+Comment.all = []
+
+
+//
+// new Comment(comment, imageId)
+  // should initialize with an id, image object (findImage) and commentContent (the actual text of the comment)
+  // should save new comment to Comment.all property
+// Comment.all
+  // should return all of the comment objects in an array
+  // a property of the Comment class
+// Comment.prototype.findImage(imageId)
+  // given an int for an image id, returns the image object with that id
+  // before return - adds current comment to image's comments property
+// Comment.prototype.commentEl()
+  // returns a string of html
+  // html has an li tag with an id field and shows the comment

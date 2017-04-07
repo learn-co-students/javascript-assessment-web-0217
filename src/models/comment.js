a9a43303525c074046244a3f11f70b2c43da4778
James Phan <jamesvphan@gmail.com>
2017-04-07 11:30:15 -0400

    Complete sort of

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,30 @@
 // create Comment class here
+const commentArray = []
+
+class Comment {
+  constructor(comment, image){
+    this.commentContent = comment
+    this.image = this.findImage(image)
+    commentArray.push(this)
+  }
+
+  static all() {
+    return commentArray
+  }
+
+  findImage(image) {
+    let foundImage = Image.all.find(function(img) {
+      return img.url === image.src
+    })
+    foundImage.comments.push(this.commentContent)
+    return foundImage
+  }
+
+  commentEl() {
+    let allComments = this.image.comments.slice(-1)
+    
+    return allComments.map(function(comment) {
+      return `<li> ${comment} </li>`
+    })
+  }
+}

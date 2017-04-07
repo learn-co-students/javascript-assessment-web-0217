e15b9d7f97c6f4520af5853c0fbaf6962224c336
github username <github email address>
2017-04-07 11:13:33 -0400

    done

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -4,10 +4,25 @@ class CommentsController {
   }
 
   init() {
-    // kick off controller from here
+    this.addCommentFormListener()
+  }
+
+  static render(commentObject) {
+    return $(`#comments-${commentObject.imageId}`).append(commentObject.commentEl())
   }
 
   addCommentFormListener() {
-    // create comment form listener code here
+    $('.add-comment').each(function() {
+      $(this).on('submit', function(e) {
+        e.preventDefault()
+        let imageId = this.dataset.id
+        let content = $(`#comment-description-${imageId}`).val()
+        var newComment = new Comment(content, imageId)
+        CommentsController.render(newComment)
+      })
+    })
   }
+
+
+
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,27 @@
-// create Comment class here
+const allComments = []
+
+class Comment {
+  constructor(comment, imageId) {
+    this.id = allComments.length,
+    this.comment = comment
+    this.imageId = imageId
+    allComments.push(this)
+  }
+
+  static all() {
+    return allComments
+  }
+
+  findImage(int) {
+    let allImages = Image.all()
+    let obj = allImages.filter(function ( obj ) {
+      return obj.id === int;
+      })[0];
+    return obj
+  }
+
+  commentEl() {
+    return `<li id=${this.id}>${this.comment}</li>`
+  }
+
+}

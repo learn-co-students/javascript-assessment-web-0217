f459d13f4f57bf6a4dab4095723217e1d68ef7e0
Noah Schechter <noahschechter7@gmail.com>
2017-04-07 10:23:00 -0400

    added basic functionality

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,13 +1,26 @@
 class CommentsController {
-  constructor() {
-    this.$addCommentForm = $('.add-comment')
-  }
+	constructor() {
+		this.$addCommentForm = $('.add-comment')
+		this.addCommentFormListener();
+	}
 
-  init() {
-    // kick off controller from here
-  }
+	init() {
+	}
 
-  addCommentFormListener() {
-    // create comment form listener code here
-  }
+	addCommentFormListener() {
+		this.$addCommentForm.each((index, form) => {
+			form.addEventListener("submit", (event) => {
+				event.preventDefault();
+				let parentInfo = $(event)[0].target.parentNode;
+				let value = event.currentTarget.children[1].value;
+				commentsController.render(new Comment(value, parentInfo.id))
+			})
+		});
+	}
+
+	render(commentObj) {
+		let id = commentObj.id;
+		let $commentListSelector = $(`#${id} ul`);
+		$commentListSelector.append(commentObj.commentEl());
+	}
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,25 @@
 // create Comment class here
+var comments = []
+
+class Comment {
+	constructor(comment, imageId) {
+		this.id = imageId;
+		this.image = this.findImage(imageId);
+		this.commentContent = comment;
+		comments.push(this);
+	}
+
+	static all() {
+		return comments;
+	}
+
+	findImage(imageId) {
+		return Image.all.find((image) => {
+			return image.imageId === imageId
+		});
+	}
+
+	commentEl() {
+		return "<li id=" + this.id + ">" + this.commentContent + "</li>"
+	}
+}

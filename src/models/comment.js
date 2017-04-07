924eda8e0bec824673319afa18ba20c6db7899a9
chasms <chasms@chas.ms>
2017-04-07 10:05:44 -0400

    added comment class

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,26 @@
-// create Comment class here
+'use strict';
+// CommentModel
+
+function Comment(comment, imageId) {
+  this.id = this.constructor.all.length
+  this.commentContent = comment
+  this.image = this.findImage(imageId)
+  this.constructor.all.push(this)
+}
+
+Comment.all = []
+
+Comment.prototype.findImage(id) {
+  let image = Image.all[id]
+  if (image) {
+    image.comments.push(this)
+    return image
+  }
+  return
+}
+
+Comment.prototype.commentEl() {
+  return `<li class="image-comment image-${this.image.id}-comment" id="image-${this.image.id}-comment-${this.id}">
+            <p>${this.commentContent}</p>
+          </li>`
+}

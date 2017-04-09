519bfb0b337aa8637ca6582c6cb1cfc217170170
gee2789 <geeyung.li@gmail.com>
2017-04-09 16:53:33 -0400

    most stuff

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,26 @@
 // create Comment class here
+class Comment {
+  constructor (imageId, comment){
+    this.id = this.constructor.all.length
+    this.commentContent = comment
+    this.imageObject = this.findImage(imageId)
+    this.comment = comment
+    this.constructor.all.push(this)
+  }
+}
+
+Comment.prototype.commentEl = () => {
+  return `<div class="comment">
+  <li id=comment-${this.id}>
+  ${this.comment}
+  </li>`
+}
+
+Comment.all = []
+
+Comment.prototype.findImage = (imageID) => {
+  let id = parseInt(imageID)
+  let imageObject = Image.all.find(id)
+  imageObject.comments.push(this.comment)
+  return Image.all.find(id)
+}
